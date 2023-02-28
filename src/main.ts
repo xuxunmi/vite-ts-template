8d9419ad478ca8a7b8d0b3afd720a01f2eb74e58
xuxunmi <pt-xxm@pt-china.com.cn>
2023-02-28 15:40:39 +0800

    feat:20230228:pinia:持久化插件pinia-plugin-persistedstate使用

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -9,7 +10,8 @@ import "tailwindcss/tailwind.css"
 
 const app = createApp(App)
 
-const pinia = createPinia();
+const pinia = createPinia().use(piniaPluginPersistedstate);
+
 app.use(pinia)
 
 app.use(router)
--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
     state: () => {
@@ -9,13 +9,19 @@ export const useUserStore = defineStore('user', {
     getters: {},
     actions: {
         setToken(token: string) {
-            console.log('传参token: ', token);
+            console.log('传参token: ', token)
             this.token = token
         }
         // 异步需要搭配 async await 语法
-        // async getTokenAsync() { 
+        // async getTokenAsync() {
         //     const result = await login()
         //     this.token = result
         // }
+    },
+    // persist: true // 开启持久化
+    persist: {
+        key: 'user', // store.$id 作为存储的默认键，即 defineStore 的第一个参数。
+        storage: window.localStorage, // 修改为 sessionStorage，默认为 localStorage
+        paths: undefined // 持久化属性数组, [] 表示没有状态被持久化， undefined 或 null 表示整个状态被持久化。
     }
 })
