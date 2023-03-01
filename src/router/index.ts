9915bbc6127eefeb7f2722ad4f8597462d6e9ec7
xuxunmi <pt-xxm@pt-china.com.cn>
2023-03-01 17:26:22 +0800

    feat:20230301:vue-router:滚动行为

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,36 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+    // {
+    //     path: '/',
+    //     redirect: '/home'
+    // },
+    // {
+    //     path: '/',
+    //     name: 'home',
+    //     component: HomeView
+    // },
+    // {
+    //     path: '/about',
+    //     name: 'about',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (About.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () => import('../views/AboutView.vue')
+    // }
+]
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL), // hash模型
-    routes: [
-        // {
-        //     path: '/',
-        //     redirect: '/home'
-        // },
-        // {
-        //     path: '/',
-        //     name: 'home',
-        //     component: HomeView
-        // },
-        // {
-        //     path: '/about',
-        //     name: 'about',
-        //     // route level code-splitting
-        //     // this generates a separate chunk (About.[hash].js) for this route
-        //     // which is lazy-loaded when the route is visited.
-        //     component: () => import('../views/AboutView.vue')
-        // }
-    ]
+    // 滚动行为
+    scrollBehavior: (to, from, savePosition) => {
+        if (savePosition) {
+            return savePosition
+        } else {
+            return { left: 0, top: 0 }
+        }
+    },
+    routes
 })
 
 export default router
