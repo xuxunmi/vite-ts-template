53b0de912b169a42887863aafd1580ae15651a98
xuxunmi <pt-xxm@pt-china.com.cn>
2023-12-12 11:43:24 +0800

    feat:20231212:监听浏览器刷新/f5刷新,重新获取路由和按钮权限，防止重新登录

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 <script setup lang="ts">
 import layout from '@/views/Layout/index.vue'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// import { useRoute } from "vue-router"
+
+// const route = useRoute()
 
 // 中文国际化
 const locale = zhCn
@@ -26,6 +29,25 @@ const reload = (): void => {
 provide('reload', reload)
 
 console.log('VITE_PORT环境变量: ', import.meta.env.VITE_PORT)
+
+// const handleRefresh = async () => {
+//     try {
+//         // 重新获取路由
+//         const data = await permissionStore.getDynamicRoutes(true)
+//         await permissionStore.setRoutes(data)
+//         // 重新获取按钮权限
+//         await permissionStore.getPermissionsBtn()
+//         // 无感刷新页面
+//         reload()
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// // 监听浏览器刷新/f5刷新,重新获取路由和按钮权限，防止重新登录
+// onMounted(() => {
+//     handleRefresh()
+// })
 </script>
 
 <style lang="less" scoped></style>
