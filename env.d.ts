b2321cfa831a51e2466349bfc7a3c051371fde1d
xuxunmi <pt-xxm@pt-china.com.cn>
2024-11-06 10:46:39 +0800

    优化

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,7 @@
 // 三斜线指令: 用于在 TypeScript 文件中引入 Vite 的类型声明文件
 
 declare module '*.vue' {
-    import { DefineComponent } from 'vue'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    import type { DefineComponent } from 'vue'
     const component: DefineComponent<{}, {}, any>
     export default component
 }
